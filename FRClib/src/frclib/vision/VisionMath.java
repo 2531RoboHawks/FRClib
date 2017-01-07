c0ed076e72f1f6f6ec69995890d2ab5e26a401fb
chase <chase@localhost>
2017-01-07 08:43:43 -0600

    added distance function

--- FRClib/src/frclib/vision/VisionMath.java
+++ FRClib/src/frclib/vision/VisionMath.java
@@ -4,8 +4,11 @@ import com.ni.vision.NIVision;
 
 public class VisionMath {
 
-	public static double computeDistance(NIVision.Rect rect, double fov, int sceenwidth, int screenheight) {
-
+	public static double computeDistance(NIVision.Rect rect, double fov, int screenwidth, int targetWidthFt) {
+		if (rect != null) {
+			double d = targetWidthFt * screenwidth / (2 * rect.width * Math.tan(fov));
+			return d;
+		}
 		return 0;
 	}
 }
